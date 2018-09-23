0c149947e5f65c54339b22afba834af1c6150c8a
Catherine Bronzo <cbronzo@mac.com>
2018-09-23 20:36:58 +0000

    automatically backed up by learn

---
+++ app/models/song.rb
@@ -0,0 +1,14 @@
+class Song < ActiveRecord::Base
+
+  validates :title, presence: true, uniqueness: true
+  validates :title, uniqueness: { scope: :release_year }
+  validates :released, inclusion: { in: %w(true false) }
+  validates :release_year, presence: true, if: :released?
+  validates :artist_name, presence: true
+  
+  
+  def released?
+    released == true
+  end
+  
+end
